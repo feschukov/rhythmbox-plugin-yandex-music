b063f38a3982d936ba436f80982835f9e3dc1bd8
Sergey Feschukov <snfesh@yandex.ru>
2022-04-13 19:37:56 +0500

    Вынес классы списков в отдельные файлы

---
+++ rotor_stations_dashboard.py
@@ -0,0 +1,61 @@
+from gi.repository import RB, GLib, Gdk
+
+class YMDashboardEntry(RB.RhythmDBEntryType):
+    def __init__(self, client, station):
+        RB.RhythmDBEntryType.__init__(self, name='ym-dashboard-entry', save_to_disk=False)
+        self.client = client
+        self.station = station
+        self.last_track = None
+
+    def do_get_playback_uri(self, entry):
+#        if self.last_track:
+#            self.client.rotor_station_feedback_track_finished(station=self.station, track_id=self.last_track, total_played_seconds=entry.get_ulong(RB.RhythmDBPropType.DURATION)*1000)
+        self.last_track = entry.get_string(RB.RhythmDBPropType.LOCATION)[5:]
+        downinfo = self.client.tracks_download_info(track_id=self.last_track, get_direct_links=True)
+#        self.client.rotor_station_feedback_track_started(station=self.station, track_id=self.last_track)
+        return downinfo[1].direct_link
+
+class YMDashboardSource(RB.BrowserSource):
+    def __init__(self):
+        RB.BrowserSource.__init__(self)
+
+    def setup(self, db, client, station):
+        self.initialised = False
+        self.db = db
+        self.entry_type = self.props.entry_type
+        self.client = client
+        self.station = station
+        self.last_track = None
+
+    def do_selected(self):
+        Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, self.rotor_station_tracks)
+
+    def rotor_station_tracks(self):
+        tracks = self.client.rotor_station_tracks(station=self.station, queue=self.last_track).sequence
+        self.iterator = 0
+        self.listcount = len(tracks)
+        Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, self.add_entry, tracks)
+        return False
+
+    def add_entry(self, tracks):
+        track = tracks[self.iterator].track
+        if track.available:
+            entry = RB.RhythmDBEntry.new(self.db, self.entry_type, 'feed:'+str(track.id)+':'+str(track.albums[0].id))
+            if entry is not None:
+                self.db.entry_set(entry, RB.RhythmDBPropType.TITLE, track.title)
+                self.db.entry_set(entry, RB.RhythmDBPropType.DURATION, track.duration_ms/1000)
+                artists = ''
+                for artist in track.artists:
+                    if len(artists) > 1:
+                        artists += ', '+artist.name
+                    else:
+                        artists = artist.name
+                self.db.entry_set(entry, RB.RhythmDBPropType.ARTIST, artists)
+                self.db.entry_set(entry, RB.RhythmDBPropType.ALBUM, track.albums[0].title)
+                self.db.commit()
+        self.iterator += 1
+        if self.iterator >= self.listcount:
+            self.last_track = str(track.id)+':'+str(track.albums[0].id)
+            return False
+        else:
+            return True
---
+++ user_likes.py
@@ -0,0 +1,59 @@
+from gi.repository import RB, GLib, Gdk
+
+class YMLikesEntry(RB.RhythmDBEntryType):
+    def __init__(self, client):
+        RB.RhythmDBEntryType.__init__(self, name='ym-likes-type', save_to_disk=False)
+        self.client = client
+
+    def do_get_playback_uri(self, entry):
+        track_id = entry.get_string(RB.RhythmDBPropType.LOCATION)[6:]
+        downinfo = self.client.tracks_download_info(track_id=track_id, get_direct_links=True)
+        return downinfo[1].direct_link
+
+    def do_destroy_entry(self, entry):
+        track_id = entry.get_string(RB.RhythmDBPropType.LOCATION)
+        return self.client.users_likes_tracks_remove(track_ids=track_id)
+
+class YMLikesSource(RB.BrowserSource):
+    def __init__(self):
+        RB.BrowserSource.__init__(self)
+
+    def setup(self, db, client):
+        self.initialised = False
+        self.db = db
+        self.entry_type = self.props.entry_type
+        self.client = client
+
+    def do_selected(self):
+        if not self.initialised:
+            self.initialised = True
+            Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, self.users_likes_tracks)
+
+    def users_likes_tracks(self):
+        tracks = self.client.users_likes_tracks().fetch_tracks()
+        self.iterator = 0
+        self.listcount = len(tracks)
+        Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, self.add_entry, tracks)
+        return False
+
+    def add_entry(self, tracks):
+        track = tracks[self.iterator]
+        if track.available:
+            entry = RB.RhythmDBEntry.new(self.db, self.entry_type, 'likes:'+str(track.id)+':'+str(track.albums[0].id))
+            if entry is not None:
+                self.db.entry_set(entry, RB.RhythmDBPropType.TITLE, track.title)
+                self.db.entry_set(entry, RB.RhythmDBPropType.DURATION, track.duration_ms/1000)
+                artists = ''
+                for artist in track.artists:
+                    if len(artists) > 1:
+                        artists += ', '+artist.name
+                    else:
+                        artists = artist.name
+                self.db.entry_set(entry, RB.RhythmDBPropType.ARTIST, artists)
+                self.db.entry_set(entry, RB.RhythmDBPropType.ALBUM, track.albums[0].title)
+                self.db.commit()
+        self.iterator += 1
+        if self.iterator >= self.listcount:
+            return False
+        else:
+            return True
