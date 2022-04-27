f5ceeee70fc6de0839392a3cc2f7158394742609
Sergey Feschukov <snfesh@yandex.ru>
2022-04-27 10:51:31 +0500

    Исключил повторный запрос списка треков из плейлиста

--- user_playlists.py
+++ user_playlists.py
@@ -4,3 +4,8 @@ from rotor_stations_dashboard import YMFeedSource
 class YMUserPlaylistSource(YMFeedSource):
     def load_tracks(self):
         return self.client.users_playlists(self.station[6:]).fetch_tracks()
+
+    def do_selected(self):
+        if not self.initialised:
+            self.initialised = True
+            super().do_selected()
